ac17f0282d9782029773661e817dd7febec59f16
MovilCentro <MovilCentro@users.noreply.github.com>
2017-02-10 01:31:25 -0400

    Create js-image-slider.js

---
+++ js-image-slider.js
@@ -0,0 +1,18 @@
+ var sliderOptions =
+        {
+            sliderId: "slider",
+            effect: "17,13,1",
+            effectRandom: true,
+            pauseTime: 2800,
+            transitionTime: 1200,
+            slices: 14,
+            boxes: 8,
+            hoverPause: 1,
+            autoAdvance: true,
+            captionOpacity: 0.4,
+            captionEffect: "fade",
+            thumbnailsWrapperId: "thumbs",
+            m: false,
+            license: "mylicense"
+        };
+    
